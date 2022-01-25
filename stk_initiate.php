a55a2a1b8e76438fcdc03bcf2d5d50acf82253ee
wycliffepepela <47030822+pepelawycliffe@users.noreply.github.com>
2022-01-25 09:49:07 +0300

    Update stk_initiate.php

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'Keypt7N48B9aSa7P05c3XEY9tTrMO3nw'; //Fill with your app Consumer Key
-  $consumerSecret = 'SQhtNXAk1U2ni28V'; // Fill with your app Secret
+  $consumerKey = 'WSqMl7HHbSVz1dAteuhU2Rn03Wuss2WB'; //Fill with your app Consumer Key
+  $consumerSecret = 'SjHc2QRHHVvj3cv6'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://morning-forest-72309.herokuapp.com/callback_url.php';  
+  $CallBackURL = 'https://quiet-journey-16210.herokuapp.com//callback_url.php';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
